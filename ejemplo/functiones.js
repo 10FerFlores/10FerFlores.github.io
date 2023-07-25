4dd6ce8756e11de8be69f247068005dd23c3e923
10FerFlores <140541570+10FerFlores@users.noreply.github.com>
2023-07-25 11:17:52 -0600

    Add files via upload

---
+++ ejemplo/functiones.js
@@ -0,0 +1,56 @@
+function msj_salir(){
+    //algoritmo de la función
+    alert("¡Vuelve pronto!")
+}
+
+function invitar (){
+    var nombre;
+    var deseo;
+
+    nombre=prompt ("Escribe tu nombre:")
+    deseo=prompt ("¿Qué te gusta comer?")
+
+    alert( "Hola! "+nombre+ "te invito a comer "+deseo)
+}
+
+
+function imagen (){
+    var IMAGEN;
+    var deseo;
+
+    nombre=prompt ("IMAGEN")
+    deseo=prompt ("¿Qué te gusta comer?")
+
+    alert( "Hola! "+nombre+ "te invito a comer "+deseo)
+}
+
+function imagen (){
+    var imagen;
+    var deseo;
+
+    nombre=prompt ("imagen")
+    deseo=prompt ("¿Que te gusta comer?")
+
+    alert ("hola! "+nombre+ "te invito a comer"+ deseo)
+
+
+}
+
+function adivinar(){
+    var respuestas;
+    respuesta=prompt("Para ser más pequeño que los demas, se levanta mas que todos si esta motivado y contento, ¿que es? ")
+
+    //si la respuesta es correcta
+    if(respuesta=="el dedo pulgar"){
+        alert("¡correcto!")
+    }
+    //sino
+    else{
+        alert("¡error!")
+    }
+}
+function chaneque (){
+    window.open ("https://www.youtube.com/watch?v=5VVDCNkFapw")
+}
+
+
